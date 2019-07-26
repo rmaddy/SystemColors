497fdd132d4ec768c42ccc91705ed669a8b61e5f
Rick Maddy <395819+rmaddy@users.noreply.github.com>
2019-07-25 22:47:37 -0600

    Ensure UI isn't created in AppDelegate under iOS 13. Fixed deployment target setting.

--- SystemColors/AppDelegate.swift
+++ SystemColors/AppDelegate.swift
@@ -13,13 +13,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     // For iOS 12 or earlier
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let vc = ViewController()
-        let nc = UINavigationController(rootViewController: vc)
-        let win = UIWindow(frame: UIScreen.main.bounds)
-        win.rootViewController = nc
-        win.makeKeyAndVisible()
-        window = win
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if #available(iOS 13.0, *) {
+            // no-op - UI created in scene delegate
+        } else {
+            let vc = ViewController()
+            let nc = UINavigationController(rootViewController: vc)
+            let win = UIWindow(frame: UIScreen.main.bounds)
+            win.rootViewController = nc
+            win.makeKeyAndVisible()
+            window = win
+        }
 
         return true
     }
